d7b605918d2104db3371777291d4b82a9cadda52
Nana-titu24 <natashaships@gmail.com>
2024-10-25 13:26:37 +0200

    asd

--- smartFarm/lib/main.dart
+++ smartFarm/lib/main.dart
@@ -1,28 +1,37 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'dart:io';
+import 'package:flutter_gemini/flutter_gemini.dart'; // Make sure the flutter_gemini package is installed
 import 'dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid
-      ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-            apiKey: "AIzaSyC7NHmajgeYlbU03PJ8WbOJYa4SztEa2Fw",
-            appId: "1:355240987985:android:f256be5591992a7e885770",
-            messagingSenderId: "355240987985",
-            projectId: "agrisfa",
-          ),
-        )
-      : await Firebase.initializeApp();
+  
+  // Initialize Gemini
+  Gemini.init(
+    apiKey: 'AIzaSyDZu8o67zJyLeus-kyq53EjCKocc-Wi91s',
+  );
+  
+  // Initialize Firebase
+  if (Platform.isAndroid) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyC7NHmajgeYlbU03PJ8WbOJYa4SztEa2Fw",
+        appId: "1:355240987985:android:f256be5591992a7e885770",
+        messagingSenderId: "355240987985",
+        projectId: "agrisfa",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,214 +46,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title});
-
+class MyHomePage extends StatelessWidget {
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _dobController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-
-  Future<void> _showSignupDialog() async {
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Sign Up'),
-          content: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextField(
-                  controller: _firstNameController,
-                  decoration: const InputDecoration(labelText: 'First Name'),
-                ),
-                TextField(
-                  controller: _lastNameController,
-                  decoration: const InputDecoration(labelText: 'Last Name'),
-                ),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'Email Address'),
-                ),
-                TextField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                ),
-                TextField(
-                  controller: _dobController,
-                  decoration: const InputDecoration(labelText: 'Date of Birth'),
-                ),
-                TextField(
-                  controller: _phoneController,
-                  decoration: const InputDecoration(labelText: 'Phone Number'),
-                ),
-                TextField(
-                  controller: _addressController,
-                  decoration: const InputDecoration(labelText: 'Address'),
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () async {
-                // Validate input fields here if needed
-                try {
-                  final UserCredential userCredential =
-                      await _auth.createUserWithEmailAndPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                  );
-                  // Handle successful signup
-                  // For example, you can navigate to the dashboard
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DashboardPage(),
-                    ),
-                  );
-                } catch (e) {
-                  // Handle signup errors
-                }
-              },
-              child: const Text('Sign Up'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _showLoginDialog() async {
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Login'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                controller: _emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-              ),
-              TextField(
-                controller: _passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () async {
-                // Validate input fields here if needed
-                try {
-                  final UserCredential userCredential =
-                      await _auth.signInWithEmailAndPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                  );
-                  // Handle successful login
-                  // For example, you can navigate to the dashboard
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DashboardPage(),
-                    ),
-                  );
-                } catch (e) {
-                  // Handle login errors
-                  print('Failed to login: $e');
-                }
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            'assets/images/img1.jpg', // Replace 'assets/background_image.jpg' with your actual image path
-            fit: BoxFit.cover,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.black.withOpacity(0.6),
-                  Colors.black.withOpacity(0.4),
-                  Colors.transparent,
-                ],
-                stops: const [0.0, 0.5, 1.0],
-              ),
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'WELCOME TO SMART FARM',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _showLoginDialog,
-                  child: const Text('Login'),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _showSignupDialog,
-                  child: const Text('Sign Up'),
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: Center(
+        child: Text(
+          'Welcome to SMART FARM',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
